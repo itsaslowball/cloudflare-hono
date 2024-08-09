5dcd22d48bd4b4385fcf94fe1ae0734d09ead654
Priyanshu <priyanshu.singh@contentstack.com>
2024-08-10 02:18:48 +0530

    created hono app and hosted on cloudflare

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+async function AuthMiddleware(c:any, next:any) {
+  if (c.req.header("Authorization")) {
+    await next()
+  } else {
+    return c.text("You don't have acces");
+  }
+}
+
+
+app.post('/', AuthMiddleware, async (c) => {
+  const body = await c.req.json()
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
+  return c.text('Hello Hono!')
+})
+app.get('/', async (c) => {
+  return c.text('Hello Hono!')
+ })
+
+export default app
